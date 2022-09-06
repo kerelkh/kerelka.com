d16920e4b7c52bced1616e5795f3f6d2d8ec751e
kerelkh <kerelkh@gmail.com>
2022-09-06 17:40:58 +0700

    resolve #1

--- resources/views/admin/components/nav.blade.php
+++ resources/views/admin/components/nav.blade.php
@@ -1,7 +1,7 @@
 <div data-aos="fade-left" class="absolute top-0 left-0 right-0 py-3 px-12 flex justify-end items-center space-x-5 bg-[#1B203D]">
   <p class="text-gray-50">Hi, {{ Auth::user()->name }}</p>
   <div class="rounded-full overflow-hidden border-2 flex justify-center items-center border-blue-500">
-    <img src="{{ asset('images/writer.jpg') }}" alt="author" class="h-10 w-10 object-cover">
+    <img src="{{ asset('images/writer.webp') }}" alt="author" class="h-10 w-10 object-cover">
   </div>
   <form action="{{ route('auth.logout') }}" method="POST">
     @csrf
