4b1da96e554b070ea239d3c0ef875a5996deeba4
kerelkh <kerelkh@gmail.com>
2022-09-07 10:25:53 +0700

    new UI for POST - resolve #6

--- resources/views/components/navtop.blade.php
+++ resources/views/components/navtop.blade.php
@@ -1,17 +1,18 @@
 <div id="nav" class="flex justify-between items-center py-5 border-b border-gray-300 mb-5 sm:mb-10 mx-2 sm:mx-4 md:mx-12 lg:mx-24 xl:mx-48">
   <div id="sideButton" onclick="toggleMenu()">
-    <button class="w-fit h-fit p-2 rounded-full text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-purple-500 text-xl hover:scale-110 transition ease-in-out"><i class="fa-solid fa-bars"></i></button>
+    <button class="w-fit h-fit p-2 rounded-full text-transparent bg-clip-text bg-blue-500 text-xl hover:scale-110 transition ease-in-out"><i class="fa-solid fa-bars"></i></button>
   </div>
   <a href="/">
-    <div id="logo" class="h-12">
-      <img src="{{ asset('icons/icon-black.png') }}" alt="Logo" class="h-full object-cover">
+    <div id="logo">
+      {{-- <img src="{{ asset('icons/icon-black.png') }}" alt="Logo" class="h-full object-cover"> --}}
+      <h3 class="tracking-widest uppercase font-bold text-2xl">KEREL<span class="text-blue-500">K</span>A</h3>
     </div>
   </a>
   <div class="flex md:hidden"></div>
-  <div class="hidden md:flex space-x-10 items-center text-gray-600">
-    <a href="https://www.facebook.com/kerelka" class="animate-bounce hover:text-gray-900"><i class="fa-brands fa-facebook-f"></i></a>
-    <a href="#" class="animate-bounce hover:text-gray-900"><i class="fa-brands fa-twitter"></i></a>
-    <a href="https://www.instagram.com/kerelkh/" class="animate-bounce hover:text-gray-900"><i class="fa-brands fa-instagram-square"></i></a>
+  <div class="hidden md:flex space-x-10 items-center text-gray-200">
+    <a href="https://www.facebook.com/kerelka" class="animate-bounce hover:text-gray-200"><i class="fa-brands fa-facebook-f"></i></a>
+    <a href="#" class="animate-bounce hover:text-gray-200"><i class="fa-brands fa-twitter"></i></a>
+    <a href="https://www.instagram.com/kerelkh/" class="animate-bounce hover:text-gray-200"><i class="fa-brands fa-instagram-square"></i></a>
   </div>
 </div>
 
--- resources/views/showpost.blade.php
+++ resources/views/showpost.blade.php
@@ -16,54 +16,58 @@
   <div class="grid md:grid-cols-6 mx-2 sm:mx-4 md:mx-12 lg:mx-24 xl:mx-48 space-x-5">
     <div class="col-span-1 md:col-span-4">
         <div id="post" class="flex flex-col items-center">
-            <div class="w-full flex justify-start space-x-5 items-center text-gray-400 mb-2 md:mb-5 text-xs md:text-sm lg:text-base">
+            <div class="w-full flex justify-start  space-x-2 items-center text-gray-200 mb-2 md:mb-5 text-xs md:text-sm lg:text-base">
               <a href="/" class="hover:text-gray-800">Home</a>
               <i class="fa-solid fa-caret-right"></i>
-              <a href="{{ $post->slug }}" class="text-gray-700">{{ $post->title }}</a>
+              <a href="{{ $post->slug }}" class="text-gray-200">{{ $post->title }}</a>
 
             </div>
-            <div class="text-gray-500 w-full text-xs mb-5 flex space-x-2">
+            <div class="text-gray-200 w-full text-xs mb-5 flex space-x-2  rounded-xl">
                 <p class="pr-2 sm:pr-0">{{ $post->updated_at }}</p>
-            <p class="text-gray-500 pr-2 sm:pr-0"><span class="text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-purple-500"><i class="fa-solid fa-eye"></i></span> ({{ $post->view }})</p>
+            <p class="text-gray-200 pr-2 sm:pr-0"><span class="text-transparent bg-clip-text bg-blue-500"><i class="fa-solid fa-eye"></i></span> ({{ $post->view }})</p>
             </div>
 
-
-            <div class="w-full   text-gray-800 mb-5">
-              <h1 class="text-lg sm:text-2xl md:text-3xl font-semibold">{{ $post->title }}</h1>
-            </div>
-
-            <div class="w-full mb-5 flex">
-              <div class="w-full aspect-video md:aspect-[4/2] bg-cover bg-no-repeat bg-center relative" style="background-image: url({{ asset('storage/' .$post->thumbnail) }})">
-                <div class="absolute inset-0 bg-black/20 p-2 sm:p-4 md:p-10 flex justify-end items-start">
-                  <a href="/blogs?category={{ $post->getCategory->category_name }}" class="py-1 px-5 bg-gradient-to-br from-blue-500 to-purple-500 text-gray-200 rounded-xl text-xs sm:text-sm md:text-base">{{ $post->getCategory->category_name }}</a>
+            <div class="w-full  rounded-xl mb-5">
+                <div class="w-full text-gray-200 mb-5">
+                <h1 class="text-lg sm:text-xl md:text-2xl font-semibold capitalize">{{ $post->title }}</h1>
                 </div>
-              </div>
+
+                <div class="w-full mb-5 flex">
+                <div class="w-full rounded-lg aspect-video md:aspect-[4/2] bg-cover bg-no-repeat bg-center relative" style="background-image: url({{ asset('storage/' .$post->thumbnail) }})">
+                    <div class="absolute inset-0 bg-black/20 p-2 sm:p-4 md:p-10 flex justify-end items-start">
+                    <a href="/blogs?category={{ $post->getCategory->category_name }}" class="py-1 px-5 bg-gradient-to-br from-blue-500 to-purple-500 text-gray-200 rounded-xl text-xs sm:text-sm md:text-base">{{ $post->getCategory->category_name }}</a>
+                    </div>
+                </div>
+                </div>
             </div>
 
             <div class="w-full pb-5 sm:pb-10 border-b border-gray-300">
-              <p class="text-gray-400 text-sm sm:text-base md:text-lg">{{ $post->description }}</p>
+              <p class="text-gray-300 text-sm sm:text-base md:text-lg">{{ $post->description }}</p>
             </div>
 
             <div class="w-full py-1 flex space-x-5 mb-5 md:mb-10">
-              <a href="#" class="text-gray-400 hover:text-gray-900"><i class="fa-brands fa-facebook-f"></i></a>
-              <a href="#" class="text-gray-400 hover:text-gray-900"><i class="fa-brands fa-twitter"></i></a>
-              <a href="#" class="text-gray-400 hover:text-gray-900"><i class="fa-brands fa-instagram-square"></i></a>
+              <a href="#" class="text-gray-200 hover:text-white"><i class="fa-brands fa-facebook-f"></i></a>
+              <a href="#" class="text-gray-200 hover:text-white"><i class="fa-brands fa-twitter"></i></a>
+              <a href="#" class="text-gray-200 hover:text-white"><i class="fa-brands fa-instagram-square"></i></a>
             </div>
 
-            <div class="w-full tracking-wider first-letter:text-5xl text-gray-600 first-letter:float-left first-letter:uppercase pb-10 border-b border-gray-300 mb-5 text-sm sm:text-base md:text-lg">
+            <div class="w-full p-5 bg-gradient-to-r from-secondary to-primary ring-1 ring-blue-400/20 rounded-3xl tracking-wide first-letter:text-5xl text-gray-300 first-letter:float-left first-letter:uppercase pb-10 mb-5 text-sm sm:text-base md:text-lg">
               {!! $post->post !!}
             </div>
 
           </div>
     </div>
-    <div class="relative col-span-1 md:col-span-2 overflow-x-hidden">
-        <div class="absolute left-0 top-0 right-0 text-gray-400 mb-2 md:mb-5 text-xs md:text-sm lg:text-base px-4">
+    <div class="col-span-1 md:col-span-2 h-full overflow-x-hidden p-5">
+        <div class="w-full sticky top-0 mx-auto text-gray-400 mb-2 md:mb-5 text-xs md:text-sm lg:text-base px-4">
             <div id="avatar" class="hidden sm:block w-full aspect-square rounded-xl overflow-hidden my-5">
-                <img src="{{ asset('images/writer.webp') }}" alt="" class="h-full w-full object-cover">
+                <img src="{{ asset('images/writer.webp') }}" alt="" class="w-full h-full object-cover">
             </div>
-            <hr>
-            <p>Author</p>
-            <p class="mt-2 text-lg text-gray-800">{{ $post->author }} <span class="text-sm text-gray-500">{{ '(' . now()->year - 1997 . 'yo)' }}</span></p>
+            <div class="w-3/4 px-5">
+                <p>Author</p>
+                <p class="mt-2 text-lg text-gray-200">{{ $post->author }}</p>
+                <p class="text-sm text-gray-300">@kerelkh</p>
+                <p class="mt-2 text-gray-300 text-sm italic">Web Developer, based in Indonesia.</p>
+            </div>
         </div>
     </div>
   </div>
